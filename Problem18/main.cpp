1245354400b5a19a743c15cde7b6aa177795f9ab
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2016-10-06 18:26:50 +0800

    Completed Problem18

--- Problem18/main.cpp
+++ Problem18/main.cpp
@@ -28,5 +28,70 @@ NOTE: As there are only 16384 routes, it is possible to solve this problem by tr
 However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
 
 Date : 6 Oct 2016
-Duration : 
+Duration : 45 min
 */
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+	// Strategy: Bottom up approach. At each node, find its maximum value (best path below it), accumulating upwards till the top. 
+	// Data structure choice: 2D vector (sparse, half-filled)
+
+	vector<vector<int>> data;	// read from "input.txt" file
+	string filename = "../../input.txt";
+	fstream fileStream(filename, ios::in);
+	
+	// read input data
+	if (fileStream.is_open())
+	{
+		string aLine, aNum;
+		stringstream ss;
+		while (getline(fileStream, aLine))
+		{
+			vector<int> lineInput;
+			ss.clear();
+			ss.str(aLine);
+
+			while (getline(ss, aNum, ' '))
+			{
+				lineInput.push_back(stoi(aNum));
+			}
+			data.push_back(lineInput);
+		}
+		fileStream.close();
+	}
+	else
+	{
+		cout << "Error: Can't open input file" << endl;
+		return -1;
+	}
+	cout << "Input data : " << endl;
+	for (int i = 0; i < data.size(); i++)
+	{
+		for (int j = 0; j < data[i].size(); j++)
+			cout << data[i][j] << " ";
+		cout << endl;
+	}
+
+	for (int j=data.size()-2; j>=0; j--)
+		for (int i = 0; i < data[j].size(); i++)	// updating the maximum sum bottom up
+		{
+			data[j][i] += data[j+1][i] > data[j+1][i+1] ? data[j+1][i] : data[j+1][i+1];
+		}
+
+	cout << "Output : " << endl;
+	for (int i = 0; i < data.size(); i++)
+	{
+		for (int j = 0; j < data[i].size(); j++)
+			cout << data[i][j] << " ";
+		cout << endl;
+	}
+
+	cout << "The maximum total from top to bottom of the triangle is " << data[0][0] << endl;
+	return 0;
+}
