ffeae5b996d3ae463a8a8c6e40ef8b4e0bc7d5eb
Like Gobeawan <gobeawanl@ihpc.a-star.edu.sg>
2016-10-19 16:33:08 +0800

    Completed Problem22

--- Problem22/main.cpp
+++ Problem22/main.cpp
@@ -4,8 +4,9 @@ Using names.txt (right click and 'Save Link/Target As...'), a 46K text file cont
 For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
 What is the total of all the name scores in the file?
 
-Date : 10 Oct 2016
-Duration : 
+Date : 10-19 Oct 2016
+Duration : 5 hours
+Note: Quicksort is too slow for large list (over 5 minutes and still running, O(n^2)). Try merge sort or heapsort with much better time performance (O(n log n)). 
 */
 
 #include <iostream>
@@ -13,6 +14,92 @@ Duration :
 #include <string>
 #include <vector>
 using namespace std;
+
+int partition(vector<string> &list, int start, int end)
+{
+	int pivot = start;
+	string temp, pivotValue = list[end];
+
+	for (int i = start; i < end; i++)
+	{
+		// comparison
+		if (list[i].compare(pivotValue) > 0)
+		{
+			// swap
+			temp = list[i];
+			list[i] = list[pivot];
+			list[pivot] = temp;
+		}
+		else
+		{
+			pivot++;
+		}
+	}
+	// swap
+	list[end] = list[pivot];
+	list[pivot] = pivotValue;
+
+	return pivot;
+}
+
+void quicksort(vector<string> &list, int start, int end)
+{
+	if (start >= end)
+		return;
+
+	int pivot = partition(list, start, end);
+	quicksort(list, 0, pivot - 1);
+	quicksort(list, pivot + 1, end); 
+}
+
+void merge(vector<string> &list, int start, int mid, int end)
+{
+	vector<string> tempList;
+	int i = start, j = mid;
+	while (i < mid && j < end)
+	{
+		if (list[i] <= list[j])
+		{
+			tempList.push_back(list[i++]);
+		}
+		else
+		{
+			tempList.push_back(list[j++]);
+		}
+	}
+	int tempStart = (i == mid) ? j : i, tempEnd = (i == mid) ? end : mid;
+	for (int k = tempStart; k < tempEnd; k++)
+		tempList.push_back(list[k]);
+
+	// copy result to the list
+	for (int k = 0; k < tempList.size(); k++)
+	{
+		list[start++] = tempList[k];
+	}
+}
+
+void mergesort(vector<string> &list, int start, int end)
+{
+	if (end-start < 2)
+		return;
+
+	int mid = (start + end) / 2;
+	mergesort(list, start, mid);
+	mergesort(list, mid, end);
+
+	merge(list, start, mid, end);
+}
+
+int score(string aName)
+{
+	int score = 0;
+	for (int i = 0; i < aName.length(); i++)
+	{
+		score += aName[i] - 'A' + 1;
+	}
+
+	return score;
+}
 
 int main()
 {
@@ -35,12 +122,19 @@ int main()
 		cout << "Error in opening input file" << endl;
 	}
 
+	//quicksort(nameList, 0, nameList.size() - 1);
+	mergesort(nameList, 0, nameList.size());
+
+	for (int i = 0; i < nameList.size(); i++)
+		cout << nameList[i] << endl;
+
+	unsigned long long sum = 0;
 	for (int i = 0; i < nameList.size(); i++)
 	{
-		cout << nameList[i] << endl;
+		sum += score(nameList[i]) * (i+1);
 	}
 
-
+	cout << "The total of all the name scores in the file is " << sum << endl;
 
 	return 0;
 }
