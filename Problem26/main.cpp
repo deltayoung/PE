62f260a56ff8d059c92a5970704ec6e654441eab
kayu cawan <kayu.cawan@gmail.com>
2016-10-26 18:32:10 +0800

    Completed Problem26

--- Problem26/main.cpp
+++ Problem26/main.cpp
@@ -15,7 +15,7 @@ Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be see
 Find the value of d < 1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
 
 Date : 26 Oct 2016
-Duration : 
+Duration : 1 hour
 Strategy: Do the division operation manually to keep track of the remainders of the divisions; once the same remainder comes up again, that's when a pattern finished running and would restart.
 */
 
@@ -26,19 +26,25 @@ using namespace std;
 int main()
 {
 	const int maxDenominator = 1000;
-	vector<int> remainderList;
+	vector<int> remainderList,	// list of remainders that have occurred - as soon as one of the remainders re-occurs, it indicates a pattern is going to repeat 
+				patternBookmark; // list of bookmarked patternCounts at which a corresponding remainder happened for the first time - the length of the pattern is the difference of current patternCounter value with the bookmarked value 
 	int curRemainder, patternCounter, maxPatternLength=0, maxPatternLengthDenominator;
 	bool breakCycle;
 	for (int i = 2; i < maxDenominator; i++)
 	{
 		breakCycle = false;
-		patternCounter = -1;
+		patternCounter = 0;
 		remainderList.clear();
+		patternBookmark.clear();
 		curRemainder = 1;
 		remainderList.push_back(curRemainder);
+		patternBookmark.push_back(patternCounter);
 
 		while (!breakCycle)
 		{
+			if (curRemainder / i == 0)
+				curRemainder *= 10;	// no increment for patternCounter for the first multiplication
+
 			while (curRemainder / i == 0)
 			{
 				curRemainder *= 10;
@@ -49,23 +55,35 @@ int main()
 			patternCounter++;
 
 			curRemainder = curRemainder % i;
+			if (curRemainder == 0)
+			{
+				break;	//from while (!breakCycle) loop
+			}
 			for (int j = 0; j < remainderList.size(); j++)
 			{
 				if (curRemainder == remainderList[j])	// a pattern will repeat
 				{
-					if (patternCounter > maxPatternLength)
+					if (patternCounter-patternBookmark[j] > maxPatternLength)
 					{
 						maxPatternLengthDenominator = i;
-						maxPatternLength = patternCounter;
+						maxPatternLength = patternCounter - patternBookmark[j];
 					}
 					breakCycle = true;
 					break;
 				}
 			}
+			if (!breakCycle)
+			{
+				remainderList.push_back(curRemainder);
+				patternBookmark.push_back(patternCounter);
+			}
+			
+			//patternCounter++;
 
-			patternCounter++;
 		}
 	}
+
+	cout << "The value of d < " << maxDenominator << " for which 1/d contains the longest recurring cycle in its decimal fraction part is " << maxPatternLengthDenominator << endl;
 
 	return 0;
 }
